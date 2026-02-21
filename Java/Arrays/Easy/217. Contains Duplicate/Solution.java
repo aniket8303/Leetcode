e4309b00e3ffa341778179428c538c78fa49047a
Aniket Parekar <you@example.com>
2026-02-21 21:13:19 +0530

    Added Contains Duplicate

---
+++ Java/Arrays/Easy/217. Contains Duplicate/Solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution{
+	public static void main(String[] args) {
+		System.out.println(containsDuplicate(new int[]{1,2,3,1}));
+		System.out.println(containsDuplicate(new int[]{1,2,3,4}));
+		System.out.println(containsDuplicate(new int[]{1,1,1,3,3,4,3,2,4,2}));
+	}
+	public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i] == nums[i-1]) return true;
+        }
+     
+        return false;
+        
+    }
+}
