a17650537947d4bdc8773ee961ff843e11e6418c
Aniket Parekar <you@example.com>
2026-02-22 18:06:51 +0530

    Added reverse integer

---
+++ Java/Math/Medium/7. Reverse Integer/Solution.java
@@ -0,0 +1,17 @@
+class Solution{
+	public static void main(String[] args) {
+		System.out.println(reverse(123));
+		System.out.println(reverse(-123));
+		System.out.println(reverse(120));
+	}
+	public static int reverse(int x) {
+        int ans =0;
+        while(x!=0){
+            int digit = x%10;
+            x /= 10;
+            if(ans>Integer.MAX_VALUE/10 || ans<Integer.MIN_VALUE/10) return 0;
+            ans = ans*10 + digit;
+        }
+        return ans;
+    }
+}
